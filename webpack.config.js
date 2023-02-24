f0577e7eb742534855df03c96847ac605d2180d3
Natali Kirillova <nata.kirillova1994.com>
2023-02-24 20:57:56 +0300

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     entry: './src/index.js',
     module: {
-        rules: [{ test: /\.css$/, use: 'css-loader' }],
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
     },
 }
